ddfa18b8ab9a46d828dea9ce850cf3845fa239eb
Matias De lellis <mati86dl@gmail.com>
2022-02-01 18:52:24 -0300

    Small hack to fix segfault on Debian Buster...
    However it must be activated by patch, due the version of appstream in
    Debian Buster does not have the functions to check the version at
    compile time.

--- src/software/gpk-as-store.c
+++ src/software/gpk-as-store.c
@@ -41,10 +41,15 @@ G_DEFINE_TYPE (GpkAsStore, gpk_as_store, G_TYPE_OBJECT)
 gboolean
 gpk_as_store_load (GpkAsStore *store, GCancellable *cancellable, GError **error)
 {
+// FIXME: The debian buster package segfault with this code.
+#if 1//AS_CHECK_VERSION(0,12,6)
+	gchar **pkgnames = NULL;
+	guint p = 0;
+#endif
 	GPtrArray *components = NULL;
 	AsComponent *component = NULL;
-	gchar **pkgnames = NULL;
-	guint i = 0, p = 0;
+	const gchar *pkgname = NULL;
+	guint i = 0;
 
 	if (!as_pool_load (store->as_pool, cancellable, error))
 		return FALSE;
@@ -52,13 +57,23 @@ gpk_as_store_load (GpkAsStore *store, GCancellable *cancellable, GError **error)
 	components = as_pool_get_components (store->as_pool);
 	for (i = 0; i < components->len; i++) {
 		component = AS_COMPONENT (g_ptr_array_index (components, i));
+// FIXME: The debian buster package segfault with this code.
+#if 1//AS_CHECK_VERSION(0,12,6)
 		pkgnames = as_component_get_pkgnames (component);
 		for (p = 0; pkgnames[p] != NULL; p++) {
+			pkgname = pkgnames[p];
+#else
+		pkgname = as_component_get_pkgname (component);
+		if (pkgname) {
+#endif
 			g_hash_table_insert (store->packages_components,
-			                     pkgnames[p],
+			                     pkgname,
 			                     g_object_ref (component));
 		}
 	}
+
+	g_debug ("Appstream components: %u", components->len);
+	g_debug ("Appstream packages: %u", g_hash_table_size(store->packages_components));
 
 	return TRUE;
 }
